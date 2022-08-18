278d37e1fc271d8011897d10046f4c5965147448
申思维 <shensiwei@sina.com>
2022-08-18 14:36:32 +0800

    传入了参数

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -15,7 +15,7 @@ class Square extends React.Component {
 //Board React 组件
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
