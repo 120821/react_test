d58b1b246b7d3b4a20212422c9bd315159e409a1
申思维 <shensiwei@sina.com>
2022-08-18 15:42:03 +0800

    完善Game 组件的 render 方法，将代码从始终根据最后一次移动渲染修改为根据当前 stepNumber 渲染

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -67,6 +67,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -80,7 +81,7 @@ class Game extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ?        'Go to move #' + move :        'Go to game start';
