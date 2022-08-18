f0b504f4cfc4e2407c0174321025e2655dc8f25f
申思维 <shensiwei@sina.com>
2022-08-18 12:30:29 +0800

    puts hello world

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Hello World</h2>
+      </div>
+      <p className="App-intro">
+        你可以在 <code>src/App.js</code> 文件中修改。
+      </p>
     </div>
   );
 }
