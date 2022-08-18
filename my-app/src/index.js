78c14e9cb8568a7e1c8d8af045bd0724de63cad4
申思维 <shensiwei@sina.com>
2022-08-18 15:11:33 +0800

    在 Game 组件的构造函数中初始化 state

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -67,6 +67,15 @@ class Board extends React.Component {
 
 //Game React 组件
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
   render() {
     return (
       <div className="game">
