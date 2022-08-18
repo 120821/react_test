1c9266616e69ed90942b0198f8554c865d6f78be
申思维 <shensiwei@sina.com>
2022-08-18 14:53:59 +0800

    从 Board 组件向 Square 组件中传递两个 props 参数：value 和 onClick

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,22 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //Square  React 组件
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
+class Square extends React.Component {  render() {    return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
-      >
-        {this.state.value}
-      </button>
+        onClick={() => this.props.onClick()}      >
+        {this.props.value}      </button>
     );
   }
 }
