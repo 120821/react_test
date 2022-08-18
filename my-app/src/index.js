b201e2e046bd16a4889e0cef49a80a6bbd233b11
申思维 <shensiwei@sina.com>
2022-08-18 14:48:34 +0800

    为board组件组件初始值

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -24,6 +24,12 @@ class Square extends React.Component {
 
 //Board React 组件
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
     return <Square value={i} />;
   }
